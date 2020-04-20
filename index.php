3a116b7825a85934386783757d39714773dc437c
Rachel Johnson <rachelktyjohnson@gmail.com>
2020-04-20 12:20:39 +1000

    :hammer: Setup

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="Tailored private guitar, bass, piano, ukulele, drums lessons">
+        <meta name="viewport" content="width=device-width">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <link rel="stylesheet" href="css/styles.css">
+    </head>
+    <body>
+
+      <h1>Johnson Music School</h1>
+
+    </body>
+</html>
